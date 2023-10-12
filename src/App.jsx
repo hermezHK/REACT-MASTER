6d0fc879d3961720ccb20185d9be5893d8166f0a
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-11 20:44:28 -0500

    feat: initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { book, briefcase, shoppingBag, signal } from "./assets/icons";
+
+export default function App() {
+  return (
+    <>
+      <h1>Manage your time well</h1>
+      <div>
+        <div>
+          <img src={briefcase} alt="" />
+          <p>Work</p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// este archivo se va encarga de exportar todos los componentes creados
+
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
