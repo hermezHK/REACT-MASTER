e3079a07f8d3ae2cf61679da2da56605558564e4
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-11 22:46:04 -0500

    feat: add  style TodoListCategory

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { categories } from "./core/category";
 
 export default function App() {
   return (
-    <>
-      <h1>Manage your time well</h1>
-      <div>
-        <div>
+    <div className="p-6">
+      <h1 className="text-2xl">Manage your time well</h1>
+      <div className="my-6">
+        <h2 className="font-semibold">Categories</h2>
+        <div className="flex mt-3 justify-between">
           {categories.map((category) => (
-            <TodoListCategory icon={category.icon} text={category.text} />
+            <TodoListCategory
+              key={category.text}
+              icon={category.icon}
+              text={category.text}
+            />
           ))}
         </div>
       </div>
-    </>
+      <h2 className="font-semibold">You have 10 task for today</h2>
+    </div>
   );
 }
--- src/components/TodoListCategory/index.jsx
+++ src/components/TodoListCategory/index.jsx
@@ -2,10 +2,14 @@ import PropTypes from "prop-types";
 
 export default function TodoListCategory({ icon, text }) {
   return (
-    <div>
-      <img src={icon} alt="" />
-      <p>{text}</p>
-    </div>
+    <button className="grid place-items-center cursor-pointer">
+      <img
+        src={icon}
+        alt=""
+        className="w-[51px] h-[51px] p-2 shadow rounded-md bg-white"
+      />
+      <p className="mt-2 text-gray-400">{text}</p>
+    </button>
   );
 }
 
