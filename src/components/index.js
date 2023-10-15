b2cc348230c56c3c0717402f9219f3c629675cb9
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-15 00:53:17 -0500

    feat: add component createtask

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 // este archivo se va encarga de exportar todos los componentes creados
 
+export { default as CreateTask } from "./CreateTask";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Modal } from "./Modal";
