894d9121d8598b1b906923cc908bfcebe1051c3f
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-17 17:55:47 -0500

    feat: select order

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as CreateTask } from "./CreateTask";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Modal } from "./Modal";
+export { default as Select } from "./Select";
 export { default as TodoListCategory } from "./TodoListCategory";
 export { default as TodoList } from "./TodoList";
