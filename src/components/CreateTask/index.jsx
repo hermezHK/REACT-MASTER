5a009e3d7e58aa1ac03e2627e8273f71ea39f4d8
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-16 23:12:56 -0500

    feat: add select List options

--- src/components/CreateTask/index.jsx
+++ src/components/CreateTask/index.jsx
@@ -1,13 +1,52 @@
 import { useState } from "react";
 import { Modal } from "../../components";
+import { Listbox } from "@headlessui/react";
+import { categories } from "../../core/category";
 
 export default function CreateTask() {
   const [open, setOpen] = useState(false);
 
+  const [selectCategory, setSelectCategory] = useState(categories[0]);
+
   return (
     <>
       <Modal open={open} onClose={() => setOpen(!open)}>
-        <h1>is a Modal</h1>
+        <form action="">
+          <h2>Create Task</h2>
+          <div className="my-5">
+            <div>
+              <input
+                type="text"
+                placeholder="write here"
+                className="w-full border px-2 py-3 rounded outline-none"
+              />
+            </div>
+            <div className="mt-5">
+              <Listbox value={selectCategory} onChange={setSelectCategory}>
+                <div className="relative">
+                  <Listbox.Button className="w-full flex gap-2 py-3 px-2 rounded border">
+                    <img src={selectCategory.icon} /> {selectCategory.text}
+                  </Listbox.Button>
+                  <Listbox.Options className="absolute z-10 w-full bg-white mt-1 rounded shadow">
+                    {categories.map((category) => (
+                      <Listbox.Option
+                        key={category.text}
+                        value={category}
+                        className={`${
+                          selectCategory.text === category.text
+                            ? "bg-blue-400 text-white"
+                            : "bg-white"
+                        } py-3 flex gap-2 items-center cursor-pointer px-2 hover:bg-blue-400 hover:text-white first:rounded-t last:rounded-b`}
+                      >
+                        <img src={category.icon} /> {category.text}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </div>
+              </Listbox>
+            </div>
+          </div>
+        </form>
       </Modal>
       <button
         onClick={() => setOpen(!open)}
