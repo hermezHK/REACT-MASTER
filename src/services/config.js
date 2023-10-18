c4297a402bb57df2992881f1fab28feed13fa537
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-10-17 21:39:18 -0500

    feat: add axios

---
+++ src/services/config.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { success, failed } from "./response";
+
+const config = axios.create({
+    baseURL: "https://652f13530b8d8ddac0b23298.mockapi.io/api/v1/",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+
+export async function makeHttpRequest({ method, url, id, body }) {
+    const requests = {
+        post: async () => await config.post(url, body),
+        get: async () => await config.get(url),
+        put: async () => await config.put(`${url}/${id}`, body),
+        delete: async () => await config.delete(`${url}/${id}`),
+    };
+
+    try {
+        const data = await requests[method]();
+        return success(data);
+    } catch (error) {
+        return failed(error);
+    }
+}
